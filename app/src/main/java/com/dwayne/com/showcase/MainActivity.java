9e2c6454e3d1f6ed0d864053b137faeb8d04ae3c
Dwayne <dwayne.zhangsan@gmail.com>
2020-04-25 23:22:06 +0800

    TextView多个超链接实现跳转

---
+++ app/src/main/java/com/dwayne/com/showcase/MainActivity.java
@@ -0,0 +1,24 @@
+package com.dwayne.com.showcase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author Dwayne
+ * @email dev1024@foxmail.com
+ * @time 20/4/25 23:10
+ * @change
+ * @chang time
+ * @class 演示代码
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
