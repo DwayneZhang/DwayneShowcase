da67eaf9598ec2b694bc8cb14f6f367d71e0d7ab
Dwayne <dwayne.zhangsan@gmail.com>
2020-10-15 22:17:49 +0800

    mod<WaveView> 1. 增加小船环绕圆圈效果

--- app/src/main/java/com/dwayne/com/showcase/view/WaveView.kt
+++ app/src/main/java/com/dwayne/com/showcase/view/WaveView.kt
@@ -4,7 +4,6 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.view.animation.LinearInterpolator
 import com.dwayne.com.showcase.R
@@ -24,13 +23,18 @@ class WaveView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private lateinit var paint: Paint
     private lateinit var path: Path
+    private lateinit var circlePath: Path
     private lateinit var pathMeasure: PathMeasure
+    private lateinit var circlePathMeasure: PathMeasure
     private lateinit var bitmap: Bitmap
-    private var length : Float = 0F
+    private var length: Float = 0F
+    private var circleLength: Float = 0F
     private var waveLength: Float = 400F
     private var dx: Int = 0
-    private var boatDx : Float = 0F
+    private var boatDx: Float = 0F
+    private var circleDx: Float = 0F
     private var mMatrix = Matrix()
+    private var circleMatrix = Matrix()
 
     init {
         paint = Paint()
@@ -41,6 +45,9 @@ class WaveView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         var options = BitmapFactory.Options()
         options.inSampleSize = 6
         bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_boat, options)
+
+        circlePath = Path()
+
         startAnimation()
     }
 
@@ -53,8 +60,9 @@ class WaveView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
             path.rQuadTo(halfWaveLength / 2, -150F, halfWaveLength, 0F)
             path.rQuadTo(halfWaveLength / 2, 150F, halfWaveLength, 0F)
             index += waveLength
-//            Log.i("WaveView", "index = $index")
         } while (index < width + waveLength)
+
+        circlePath.addCircle(width.div(2F), 1000F, 200F, Path.Direction.CW)
     }
 
     private fun startAnimation() {
@@ -66,7 +74,8 @@ class WaveView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         animator.addUpdateListener { animation: ValueAnimator ->
             dx = (animation.animatedValue as Int * waveLength / 1000).toInt()
             boatDx = animation.animatedValue as Int * (length - waveLength) / 1000
-            postInvalidate()
+            circleDx = animation.animatedValue as Int * circleLength / 1000
+            invalidate()
         }
         animator.start();
     }
@@ -76,13 +85,22 @@ class WaveView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         setPathData()
         pathMeasure = PathMeasure(path, false)
         length = pathMeasure.length
-//        Log.i("WaveView", "length = $length")
         mMatrix.reset()
         pathMeasure.getMatrix(boatDx, mMatrix, PathMeasure.TANGENT_MATRIX_FLAG
                 or PathMeasure.POSITION_MATRIX_FLAG)
-        mMatrix.preTranslate((-bitmap.width/2).toFloat(), (-bitmap.height).toFloat())
+        mMatrix.preTranslate((-bitmap.width / 2).toFloat(), (-bitmap.height).toFloat())
         canvas.drawPath(path, paint)
         canvas.drawBitmap(bitmap, mMatrix, paint)
+
+        circlePathMeasure = PathMeasure(circlePath, false)
+        circleLength = circlePathMeasure.length
+        circlePathMeasure.getMatrix(circleDx, circleMatrix, PathMeasure.TANGENT_MATRIX_FLAG
+                or PathMeasure.POSITION_MATRIX_FLAG)
+        circleMatrix.preTranslate((-bitmap.width / 2).toFloat(), (-bitmap.height).toFloat())
+        canvas.drawPath(circlePath, paint)
+        canvas.drawBitmap(bitmap, circleMatrix, paint)
+
+
     }
 
 
